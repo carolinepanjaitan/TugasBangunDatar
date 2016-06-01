75164233e74c7c23072ff0e202007692ca0bc484
carolinepanjaitan <carolinepanjaitan5@gmail.com>
2016-06-01 18:02:14 +0700

    Tugas

---
+++ Polymorp.java
@@ -0,0 +1,8 @@
+package com.bangundatar.abstrak;
+
+public class Polymorp {
+    void poly (BangunDatar bangundatar){
+        bangundatar.hitungLuas();
+        bangundatar.hitungKeliling();
+    }
+}
